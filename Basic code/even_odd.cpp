05f3c1f9567bc21eb318277d4be5f0a14c8590dc
Aman Pandey <pandey8450aman@gmail.com>
2026-03-27 23:15:41 +0530

    Add files via upload

---
+++ Basic code/even_odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+    int x;
+    cout<<"enter the value of x";
+    cin>>x;
+    if (x%2 ==0) {
+        cout<<"even";
+    }
+    else {
+        cout<<"odd";
+    }
+    return 0;
+    
+
+}
