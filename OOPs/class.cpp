e04eac53a76a2209698922f39597c098e0dd7b64
Aman Pandey <pandey8450aman@gmail.com>
2026-06-09 23:01:03 +0530

    Add files via upload

--- OOPs/class.cpp
+++ OOPs/class.cpp
@@ -1,8 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class calculator;
 
-int a;
-int b;
-int c;
+class Student
+{
+public:
+    string name;
+    int rollNo;
 
+    void display()
+    {
+        cout << "Student Name: " << name << endl;
+        cout << "Roll Number: " << rollNo << endl;
+    }
+};
+
+int main()
+{
+    
+    Student s1;
+
+    
+    s1.name = "Rahul";
+    s1.rollNo = 101;
+
+    
+    s1.display();
+
+    return 0;
+}
