6729a99410e7b4a3edd932ab1086066a5f1812c0
Aman Pandey <pandey8450aman@gmail.com>
2026-07-15 22:42:32 +0530

    Add files via upload

---
+++ FUNCTIONS/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no for checking primw no"<<endl;
+    cin>>n;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            cout<<"not prime"<<endl;
+            break;
+        }
+            else {
+                cout<<"prime"<<endl;
+            }
+        
+    }
+}
