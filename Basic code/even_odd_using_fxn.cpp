759dcc6df677d248d01c814f931bdb5c527a73d4
Aman Pandey <pandey8450aman@gmail.com>
2026-07-20 10:47:00 +0530

    Add files via upload

---
+++ Basic code/even_odd_using_fxn.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int even(int a)
+{
+
+    cout<<"enter the value of a";
+    cin>>a;
+    if 
+   ( a%2 ==2)
+    cout<<"even";
+    else{
+        cout<<"odd";
+    }
+}
+
+
+int main()
+{
+
+    int even();
+    return 0;
+}
+// Need to change and use two fxn
+
+
+
+
