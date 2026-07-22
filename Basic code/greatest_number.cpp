820022a324e4a1bc87a7ef5157bee2d6becfc8ae
Aman Pandey <pandey8450aman@gmail.com>
2026-07-22 22:21:35 +0530

    Add files via upload

---
+++ Basic code/greatest_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,greatest;
+    cout<<"enter the valuse of A,B and C";
+    cin>>a>>b>>c;
+    greatest=a;
+     if (b>greatest) {
+        b=greatest;}
+     
+      if (c>greatest) {
+        c=greatest;}
+     
+     cout<<"greatest no is:"<<greatest<<endl;
+     return 0;
+     }
