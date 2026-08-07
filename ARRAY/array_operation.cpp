935b59c49d11120bdf773b47c44f8c5b0ecf8b49
Aman Pandey <pandey8450aman@gmail.com>
2026-08-07 22:07:19 +0530

    Add files via upload

---
+++ ARRAY/array_operation.cpp
@@ -0,0 +1,17 @@
+// #include<iostream>
+// using namespace std;
+// class array_operation
+// {
+//     private:
+//     int arr[5];
+//     int n;
+//     public:
+//      void input(){
+//      cout<<"enter the number of elements"<<endl;
+//      cin>>n;
+//        cout<<"enter array elements";
+//        for(int i =0;i<n;i++){
+//        cin>>arr[i];
+//      }
+//     }
+// }
