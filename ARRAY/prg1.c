8f0ecfb51c0fedf959fc1b62c596c9f7310694df
Aman Pandey <pandey8450aman@gmail.com>
2026-08-10 22:21:33 +0530

    Add files via upload

---
+++ ARRAY/prg1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a=1,sum=0;5
+    while(a<=100)
+    {
+        if (a%2==0)
+        {   sum +=a;
+            printf("%d\n",sum);
+        }
+        a++;
+    }
+    
+}
